77cc258e740bd31be8829fe1ec6ed85c5bab8059
Mert Alp Taytak <mertalp.taytak@canonical.com>
2026-01-06 16:43:03 +0300

    Fix license headers

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 # Variables for paths and configuration
 
 PROJECT_ROOT := $(CURDIR)
--- scripts/import_rock.sh
+++ scripts/import_rock.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
 
 # ==============================================================================
 # Script: import_rock
